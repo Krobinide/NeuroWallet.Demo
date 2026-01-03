bc0fb9caf3fbf1fe624fef41f1967f67347c863a
Krisanahari Siva <s3941378@student.rmit.edu.au>
2026-01-03 12:29:34 +0800

    initialization

---
+++ neurowallet-lite/backend/src/main/java/com/neurowallet/NeurowalletApplication.java
@@ -0,0 +1,12 @@
+package com.neurowallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeurowalletApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(NeurowalletApplication.class, args);
+    }
+}
